1ad82429b8558aef3d15d681547a0ad5fc434761
ninehundredth <ninehundredth@gmail.com>
2018-04-05 19:49:02 +0300

    helloWorld Idea

---
+++ src/com/itcluster/helloWorld/HelloWorldApp.java
@@ -0,0 +1,11 @@
+package com.itcluster.helloWorld;
+
+public class HelloWorldApp {                        //class declare
+
+    public static void main(String[] args) {        //the first method the program begins with
+
+        System.out.println("Hello World!");      //display string
+	// write your code here
+
+    }
+}
